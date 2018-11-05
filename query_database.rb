7e0c32f65171ed062a9065b630ce8f87c1c8786a
hricha <hricha.kabir@gmail.com>
2018-11-06 00:03:17 +0530

    Code changes for supporting multiple databases.

--- query_database.rb
+++ query_database.rb
@@ -6,38 +6,46 @@ TABLE_NAME = 'employee'
 
 def query_database()
   config = YAML.load_file(File.join(__dir__, 'config.yml'))
-  username = config['username']
-  password = config['password']
-  host = config['host']
-  database = config['database']
   #create an instance of sql utility
-  sql_adapter = SQLUtil.new(username, password, host, database)
+  sql_adapter = SQLUtil.new(config)
 
   #create table in database
   puts "\n#### Create table in database ####"
   res = sql_adapter.execute_query("CREATE TABLE IF NOT EXISTS #{TABLE_NAME} (id VARCHAR(50) PRIMARY KEY NOT NULL, name VARCHAR(200), city VARCHAR(200), job VARCHAR(50) NOT NULL)")
-  puts "#{TABLE_NAME} created successfully" if res
+  if res
+    puts "#{TABLE_NAME} created successfully"
+  else
+    puts "Error in table #{TABLE_NAME} creation."
+  end
 
   #insert records into table
   puts "\n#### Insert records in table ####"
   rows = [{'id': '1', 'name': 'Hricha', 'city': 'Pune', 'job': 'IT'}, {'id': '2', 'name': 'John', 'city': 'Sydney', 'job': 'Media'}]
-  result = sql_adapter.bulk_insert(TABLE_NAME, rows)
+  result, error = sql_adapter.bulk_insert(TABLE_NAME, rows)
   if result
     puts "Record(s) inserted into table #{TABLE_NAME} successfully."
+  else
+    puts "Error in inserting records #{error}."
   end
 
   #fetch records from table
   puts "\n#### Fetch records from table ####"
-  result = sql_adapter.fetch(TABLE_NAME)
-  result.each_hash {|h| puts h} unless result.nil?
+  result, error = sql_adapter.fetch(TABLE_NAME)
+  if error.nil?
+    result.each do |row| puts row end
+  else
+    puts "Error in fetching records #{error}."
+  end
 
   #update records
   puts "\n#### Update records in table ####"
   rows = [{'id': '1', 'name': 'Hricha Kabir'}, {'id': '2', 'job': 'Software1'}]
   unique_key_columns = [:id]
-  result = sql_adapter.bulk_update(TABLE_NAME, rows, unique_key_columns)
+  result, error = sql_adapter.bulk_update(TABLE_NAME, rows, unique_key_columns)
   if result
     puts "Record(s) updated into table #{TABLE_NAME} successfully."
+  else
+    puts "Error in updating records #{error}."
   end
 
   #delete records
@@ -45,6 +53,8 @@ def query_database()
   result = sql_adapter.bulk_delete(TABLE_NAME, 'id', ['1','2', '3'])
   if result
     puts "Record(s) deleted from table #{TABLE_NAME} successfully."
+  else
+    puts "Error in deleting records #{error}."
   end
 
   puts "\n#### Close connection #####"
--- sql_utility.rb
+++ sql_utility.rb
@@ -1,31 +1,34 @@
 require 'net/http'
 require 'mysql'
+require 'sequel'
 
 class SQLUtil
   MAX_INSERTION = 500
 
-  def initialize(username, password, host, db_name)
-      #my = Mysql.new(hostname, username, password, databasename)
-      @client = Mysql.new(host, username, password, db_name)
+  def initialize(config)
+      @client = Sequel.connect(config)
   end
 
   def close_client
-    @client.close
+    @client.disconnect
   end
 
   def fetch(table_name)
     sql_str = "SELECT * FROM #{table_name}"
     begin
-      res = @client.query(sql_str)
+      res = @client.fetch(sql_str)
     rescue => e
       puts "Unable to fetch from #{table_name} : #{e}"
+      return [], e
     end
+    return res
   end
 
   def bulk_insert(table_name, rows)
-    return false if !rows.kind_of?(Array) || rows.count == 0
+    return false, "Rows data should be of array type" if !rows.kind_of?(Array) || rows.count == 0
     inserted = true
     sql_str = ""
+    error = ""
     col_names = rows.first.map {|k,v| k}
     sql_col_names = col_names.map {|col_name| "`#{col_name}`"}
     sql_insert_str = "INSERT INTO #{table_name} (#{sql_col_names.join(',')}) values"
@@ -40,27 +43,29 @@ class SQLUtil
         sql_str += ",(#{get_col_values_str(col_values)})"
       end
       if ((index+1) % MAX_INSERTION) == 0 || rows_count == (index+1)
-        inserted = execute_query(sql_str)
+        inserted, error = execute_query(sql_str)
         sql_str = ""
       end
     end
-    return inserted
+    return inserted, error
   end
 
   def bulk_update(table_name, rows, unique_key_columns)
-    return false if !rows.kind_of?(Array) || rows.count == 0
+    return false, "Rows data should be of array type" if !rows.kind_of?(Array) || rows.count == 0
     updated = true
+    error = ""
     rows.each do |row|
       sql_str = ""
       sql_str = "#{get_update_str(table_name, row, unique_key_columns)}"
-      updated = execute_query(sql_str)
+      updated, error = execute_query(sql_str)
     end
-    return updated
+    return updated, error
   end
 
   def bulk_delete(table_name, key_name, key_values)
-    return false if !key_values.kind_of?(Array) || key_values.count == 0
+    return false, "Rows data should be of array type" if !key_values.kind_of?(Array) || key_values.count == 0
     deleted = true
+    error = ""
     delete_sql_str = "DELETE FROM #{table_name} where #{key_name} in "
     values_str = ""
     values_count = key_values.count
@@ -69,20 +74,20 @@ class SQLUtil
       values_str += "'#{value}'"
       if values_str.length >= 100 || values_count == (index+1)
         sql_str = "#{delete_sql_str} (#{values_str})"
-        deleted = execute_query(sql_str)
+        deleted, error = execute_query(sql_str)
       end
     end
-    return deleted
+    return deleted, error
   end
 
   def execute_query(sql_str)
     begin
-      res = @client.query(sql_str)
+      res = @client.run(sql_str)
     rescue => e
       puts "Error in executing query #{sql_str} :: #{e}"
-      return false
+      return false, e
     end
-    return true
+    return true, res
   end
 
   def get_col_values_str(col_values)
